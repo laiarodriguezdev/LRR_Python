17b5db995ef1cb93a8269a770d731f2e845f0150
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-03-01 16:17:47 +0100

    hello world done

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#region
 @app.get("/")
 def read_root():
     return {"Hello" : "World"}
+
+#@app.get("/items/{item_id}")
+#def read_item(item_id: int, q: Union[str, None] = None);
+#    return {"item_id": item_id, "q":q}
+#endregion
